860fb5140dd5b5e65daa4fda12614dbe060f1ee5
Jason Snitker <jasonsnitker@gmail.com>
2020-04-20 10:04:16 -0500

    Read file and display each line in console with line count at the bottom.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace MobyDickProject
     {
         static void Main(string[] args)
         {
+            string filePath = @"Assets\mobydick-chapter-text.txt";
+            StreamReader file =
+                new System.IO.StreamReader(filePath);
+
+            ShowBookTextLines(file);            
+        }
+
+        public static void ShowBookTextLines(StreamReader file)
+        {
+            int counter = 0;
+            string line;
+            
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
         }
     }
 }
